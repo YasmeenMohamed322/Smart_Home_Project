115c3d111820eb77e367353a1be27af6259e1ed0
yasmeen <yasmeenm322@gmail.com>
2023-08-17 17:02:55 +0300

    uploading DIO

--- MCAL/DIO/DIO_config.h
+++ MCAL/DIO/DIO_config.h
@@ -3,4 +3,78 @@
 #ifndef DIO_CONFIG_H_
 #define DIO_CONFIG_H_
 
+#define PA_PIN0_DIR                           INPUT
+#define PA_PIN1_DIR                           INPUT
+#define PA_PIN2_DIR                           INPUT
+#define PA_PIN3_DIR                           INPUT
+#define PA_PIN4_DIR                           INPUT
+#define PA_PIN5_DIR                           INPUT
+#define PA_PIN6_DIR                           INPUT
+#define PA_PIN7_DIR                           INPUT
+
+#define PB_PIN0_DIR                           INPUT
+#define PB_PIN1_DIR                           INPUT
+#define PB_PIN2_DIR                           INPUT
+#define PB_PIN3_DIR                           INPUT
+#define PB_PIN4_DIR                           INPUT
+#define PB_PIN5_DIR                           INPUT
+#define PB_PIN6_DIR                           INPUT
+#define PB_PIN7_DIR                           INPUT
+
+#define PC_PIN0_DIR                           INPUT
+#define PC_PIN1_DIR                           INPUT
+#define PC_PIN2_DIR                           INPUT
+#define PC_PIN3_DIR                           INPUT
+#define PC_PIN4_DIR                           INPUT
+#define PC_PIN5_DIR                           INPUT
+#define PC_PIN6_DIR                           INPUT
+#define PC_PIN7_DIR                           INPUT
+
+#define PD_PIN0_DIR                           INPUT
+#define PD_PIN1_DIR                           INPUT
+#define PD_PIN2_DIR                           INPUT
+#define PD_PIN3_DIR                           INPUT
+#define PD_PIN4_DIR                           INPUT
+#define PD_PIN5_DIR                           INPUT
+#define PD_PIN6_DIR                           INPUT
+#define PD_PIN7_DIR                           INPUT
+
+//Values(high,low,pull_up,float)
+
+#define PA_PIN0_VAL                           LOW
+#define PA_PIN1_VAL                           LOW
+#define PA_PIN2_VAL                           LOW
+#define PA_PIN3_VAL                           LOW
+#define PA_PIN4_VAL                           LOW
+#define PA_PIN5_VAL                           LOW
+#define PA_PIN6_VAL                           LOW
+#define PA_PIN7_VAL                           LOW
+
+#define PB_PIN0_VAL                           LOW
+#define PB_PIN1_VAL                           LOW
+#define PB_PIN2_VAL                           LOW
+#define PB_PIN3_VAL                           LOW
+#define PB_PIN4_VAL                           LOW
+#define PB_PIN5_VAL                           LOW
+#define PB_PIN6_VAL                           LOW
+#define PB_PIN7_VAL                           LOW
+
+#define PC_PIN0_VAL                           LOW
+#define PC_PIN1_VAL                           LOW
+#define PC_PIN2_VAL                           LOW
+#define PC_PIN3_VAL                           LOW
+#define PC_PIN4_VAL                           LOW
+#define PC_PIN5_VAL                           LOW
+#define PC_PIN6_VAL                           LOW
+#define PC_PIN7_VAL                           LOW
+
+#define PD_PIN0_VAL                           LOW
+#define PD_PIN1_VAL                           LOW
+#define PD_PIN2_VAL                           LOW
+#define PD_PIN3_VAL                           LOW
+#define PD_PIN4_VAL                           LOW
+#define PD_PIN5_VAL                           LOW
+#define PD_PIN6_VAL                           LOW
+#define PD_PIN7_VAL                           LOW
+
 #endif /* DIO_CONFIG_H_ */
