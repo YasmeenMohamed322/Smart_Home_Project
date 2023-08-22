d7ec8152acb87258e12c4afc0e396b4b3672c349
yasmeen <yasmeenm322@gmail.com>
2023-08-23 00:13:54 +0300

    DC motor

--- HAL/DCMOTOR/DCMOTOR_config.h
+++ HAL/DCMOTOR/DCMOTOR_config.h
@@ -8,6 +8,10 @@
 #ifndef HAL_DCMOTOR_DCMOTOR_CONFIG_H_
 #define HAL_DCMOTOR_DCMOTOR_CONFIG_H_
 
+#define DC_IN1_PORT                        DIO_u8PORTA
+#define DC_IN2_PORT                        DIO_u8PORTA
 
+#define DC_IN1_PIN                         DIO_u8PIN0
+#define DC_IN2_PIN                         DIO_u8PIN1
 
 #endif /* HAL_DCMOTOR_DCMOTOR_CONFIG_H_ */
