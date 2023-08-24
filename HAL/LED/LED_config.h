b34b799ad3e59226a41ab948df4107c914184eb3
yasmeen <yasmeenm322@gmail.com>
2023-08-24 20:30:54 +0300

    uploading LED

--- HAL/LED/LED_config.h
+++ HAL/LED/LED_config.h
@@ -8,6 +8,9 @@
 #ifndef HAL_LED_LED_CONFIG_H_
 #define HAL_LED_LED_CONFIG_H_
 
-
+#define LED_PORTID                                        DIO_u8PORTD
+#define LED_PINID                                         DIO_u8PIN0
+#define LED_CONNECTION                                    LED_SOURCE
+#define LED_INITSTATE                                     LED_OFF
 
 #endif /* HAL_LED_LED_CONFIG_H_ */
--- HAL/LED/LED_prog.c
+++ HAL/LED/LED_prog.c
@@ -6,3 +6,92 @@
  */
 
 
+#include "../../LIB/stdTypes.h"
+#include "../../LIB/errorStates.h"
+
+#include "LED_priv.h"
+#include "LED_config.h"
+
+#include "../../MCAL/DIO/DIO_int.h"
+
+ES_t LED_enuInit(void)
+{
+	ES_t Local_enuErrorState = ES_NOK;
+
+Local_enuErrorState = DIO_enuSetPinDirection(LED_PORTID,LED_PINID,DIO_u8OUTPUT);
+if(LED_CONNECTION == LED_SINK)
+{
+	if(LED_INITSTATE == LED_ON)
+	{
+		Local_enuErrorState = DIO_enuSetPinValue(LED_PORTID,LED_PINID,DIO_u8LOW);
+	}
+	else if(LED_INITSTATE == LED_OFF)
+	{
+		Local_enuErrorState=DIO_enuSetPinValue(LED_PORTID,LED_PINID,DIO_u8HIGH);
+	}
+	else
+	{
+		return ES_OUT_OF_RANGE;
+	}
+}
+else if(LED_CONNECTION == LED_SOURCE)
+{
+if(LED_INITSTATE == LED_ON)
+    {
+        Local_enuErrorState = DIO_enuSetPinValue(LED_PORTID,LED_PINID,DIO_u8HIGH);
+	 }
+	 else if(LED_INITSTATE == LED_OFF)
+	 {
+	 	Local_enuErrorState = DIO_enuSetPinValue(LED_PORTID,LED_PINID,DIO_u8LOW);
+	 }
+	 else
+	 {
+	 	return ES_OUT_OF_RANGE;
+	 }
+		}
+else
+{
+		return ES_OUT_OF_RANGE;
+	}
+
+
+	return Local_enuErrorState;
+}
+ES_t LED_enuTurnON(u8 Copy_LedPORT,u8 Copy_LedPIN)
+{
+	ES_t Local_enuErrorState = ES_NOK;
+	if(LED_CONNECTION == LED_SINK)
+	{
+		 Local_enuErrorState = DIO_enuSetPinValue(Copy_LedPORT,Copy_LedPIN,DIO_u8LOW);
+	}
+	else if(LED_CONNECTION == LED_SOURCE)
+	{
+		 Local_enuErrorState = DIO_enuSetPinValue(Copy_LedPORT,Copy_LedPIN,DIO_u8HIGH);
+	}
+	else
+	{
+		return ES_OUT_OF_RANGE;
+	}
+
+	return Local_enuErrorState;
+}
+ES_t LED_enuTurnOFF(u8 Copy_LedPORT,u8 Copy_LedPIN)
+{
+	ES_t Local_enuErrorState = ES_NOK;
+
+		if(LED_CONNECTION == LED_SINK)
+		{
+			 Local_enuErrorState = DIO_enuSetPinValue(Copy_LedPORT,Copy_LedPIN,DIO_u8HIGH);
+		}
+		else if(LED_CONNECTION == LED_SOURCE)
+		{
+			 Local_enuErrorState = DIO_enuSetPinValue(Copy_LedPORT,Copy_LedPIN,DIO_u8LOW);
+		}
+		else
+		{
+			return ES_OUT_OF_RANGE;
+		}
+
+		return Local_enuErrorState;
+}
+
